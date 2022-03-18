d494d0da290ff2c19409e0bb0c6d903bfc6ce190
qxinGitHub <qxguge@gmail.com>
2022-03-18 15:10:02 +0800

    Update 根据番号快速搜索.user.js

--- 根据番号快速搜索.user.js
+++ 根据番号快速搜索.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         根据番号快速搜索
 // @namespace    https://github.com/qxinGitHub/searchAV
-// @version      0.8.2
+// @version      0.8.3
 // @description  标记网页上的所有番号, 在相关网站快速方便的进行搜索
 // @author       iqxin
 // @match        *://**/*
